45fdd71386a43f2a40eff737f929e8fd85cf1bbb
Éden Alencar <eden.alencar@outlook.com>
2023-09-11 09:45:06 -0300

    Implementação de API

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,79 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Categorias não encontradas");
+            }
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Categoria não encontrada");
+            }
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+            {
+                return BadRequest("Dados inválidos");
+            }
+            await _categoryService.Add(categoryDTO);
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.Id }, categoryDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.Id)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            if (categoryDTO == null)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            await _categoryService.Update(categoryDTO);
+            return Ok(categoryDTO);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Categoria não encontrada");
+            }
+            await _categoryService.Remove(id);
+            return Ok(category);
+        }
+    }
+}
---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var produtos = await _productService.GetProducts();
+            if (produtos == null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return Ok(produtos);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var produto = await _productService.GetById(id);
+            if (produto == null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO produtoDto)
+        {
+            if (produtoDto == null)
+                return BadRequest("Dados inválidos");
+
+            await _productService.Add(produtoDto);
+
+            return new CreatedAtRouteResult("GetProduct", new { id = produtoDto.Id }, produtoDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO produtoDto)
+        {
+            if (id != produtoDto.Id)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            if (produtoDto == null)
+                return BadRequest("Dados inválidos");
+
+            await _productService.Update(produtoDto);
+
+            return Ok(produtoDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductDTO>> Delete(int id)
+        {
+            var produtoDto = await _productService.GetById(id);
+
+            if (produtoDto == null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            await _productService.Remove(id);
+
+            return Ok(produtoDto);
+        }
+    }
+}
